0ab76315cca2ce6f7048f8b208abef11779b9be5
MICRO <MICRO@DESKTOP-T2LO9DK>
2023-03-18 14:06:33 -0300

    criação tela game

--- JogoDaVelha/src/jogodavelha/JogoDaVelha.java
+++ JogoDaVelha/src/jogodavelha/JogoDaVelha.java
@@ -9,7 +9,10 @@ public class JogoDaVelha {
 
     
     public static void main(String[] args) {
-        
+        TelaJogo tela = new TelaJogo();
+
+            tela.setVisible(true);
+       
     }
     
 }
